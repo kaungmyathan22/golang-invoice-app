3f6c7dd2c8ee0f9662f276c44f80bb98d6be8322
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-26 23:57:35 +0630

    feat(user): handler methods added

---
+++ app/user/handlers/user_handler.go
@@ -0,0 +1,30 @@
+package user_handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	user_models "github.com/kaungmyathan22/golang-invoice-app/app/user/models"
+)
+
+type UserHandler struct {
+	Storage *user_models.UserStorage
+}
+
+func NewUserHandler(db *user_models.UserStorage) *UserHandler {
+	return &UserHandler{Storage: db}
+}
+
+func (handler *UserHandler) GetUsersHandler(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{"message": "GetUsersHandler"})
+}
+func (handler *UserHandler) GetUserHandler(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{"message": "GetUserHandler"})
+}
+func (handler *UserHandler) CreateUserHandler(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{"message": "CreateUserHandler"})
+}
+func (handler *UserHandler) UpdateUserHandler(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{"message": "UpdateUserHandler"})
+}
+func (handler *UserHandler) DeleteUserHandler(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{"message": "DeleteUserHandler"})
+}
