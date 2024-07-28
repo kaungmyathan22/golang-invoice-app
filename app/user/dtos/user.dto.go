3a966e43ec94454490e559b10239f5ee41f056c0
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-28 23:58:00 +0630

    feat(dto): fields validaiton updatesd

--- app/user/dtos/user.dto.go
+++ app/user/dtos/user.dto.go
@@ -1,8 +1,8 @@
 package user_dto
 
 type RegisterUserDTO struct {
-	Username string `json:"username" binding:"required" valid:"sixToEightDigitAlphanumericPasswordValidator~Password must be between 6 to 8 alphanumeric characters"`
-	Password string `json:"password" binding:"required" valid:"required~Username is required"`
+	Username string `json:"username" binding:"required" valid:"required~Username is required"`
+	Password string `json:"password" binding:"required" valid:"sixToEightDigitAlphanumericPasswordValidator~Password must be between 6 to 8 alphanumeric characters"`
 }
 
 type LoginUserDTO struct {
