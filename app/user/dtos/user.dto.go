a2d40fda9a9e98b6206131d35263e6b52a97bc2d
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-30 00:50:14 +0630

    update(): transformer added

--- app/user/dtos/user.dto.go
+++ app/user/dtos/user.dto.go
@@ -1,5 +1,10 @@
 package user_dto
 
+import (
+	"github.com/kaungmyathan22/golang-invoice-app/app/lib"
+	user_models "github.com/kaungmyathan22/golang-invoice-app/app/user/models"
+)
+
 type RegisterUserDTO struct {
 	Username string `json:"username" binding:"required" valid:"required~Username is required"`
 	Password string `json:"password" binding:"required" valid:"sixToEightDigitAlphanumericPasswordValidator~Password must be between 6 to 8 alphanumeric characters"`
@@ -7,4 +12,12 @@ type RegisterUserDTO struct {
 
 type LoginUserDTO struct {
 	RegisterUserDTO
+}
+
+func (dto *RegisterUserDTO) ToModel() (*user_models.UserModel, error) {
+	hashedPassword, err := lib.HashPassword(dto.Password)
+	if err != nil {
+		return nil, err
+	}
+	return &user_models.UserModel{Password: hashedPassword, Username: dto.Username}, nil
 }
