e0486d102216b706e3c690848f10c20f185b34ab
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-31 00:11:25 +0630

    feat(user): user entity added for http layer

---
+++ app/user/user_entity.go
@@ -0,0 +1,31 @@
+package user
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type UserEntity struct {
+	ID                    uint           `json:"id"`
+	CreatedAt             time.Time      `json:"createdAt"`
+	UpdatedAt             time.Time      `json:"updatedAt"`
+	DeletedAt             gorm.DeletedAt `json:"deletedAt"`
+	Username              string         `json:"username"`
+	Password              string         `json:"-"`
+	LastLoggedInAt        time.Time      `json:"lastLoggedInAt"`
+	LastPasswordUpdatedAt time.Time      `json:"lastPasswordUpdatedAt"`
+}
+
+func UserEntityFromUserModel(model *UserModel) *UserEntity {
+	return &UserEntity{
+		ID:                    model.ID,
+		CreatedAt:             model.CreatedAt,
+		UpdatedAt:             model.UpdatedAt,
+		DeletedAt:             model.DeletedAt,
+		Username:              model.Username,
+		Password:              model.Password,
+		LastLoggedInAt:        model.LastLoggedInAt,
+		LastPasswordUpdatedAt: model.LastPasswordUpdatedAt,
+	}
+}
