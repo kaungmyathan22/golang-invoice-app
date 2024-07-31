b8a68f107b95d3cbe6d1d726468b48e94d7f40ca
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-31 16:23:22 +0630

    feat(user): change password dto added

--- app/user/user_request.go
+++ app/user/user_request.go
@@ -9,15 +9,20 @@ type RegisterUserDTO struct {
 	Password string `json:"password" binding:"required" valid:"sixToEightDigitAlphanumericPasswordValidator~Password must be between 6 to 8 alphanumeric characters"`
 }
 
-type LoginUserDTO struct {
-	Username string `json:"username" binding:"required" valid:"required~Username is required"`
-	Password string `json:"password" binding:"required" valid:"sixToEightDigitAlphanumericPasswordValidator~Password must be between 6 to 8 alphanumeric characters"`
-}
-
 func (dto *RegisterUserDTO) ToModel() (*UserModel, error) {
 	hashedPassword, err := lib.HashPassword(dto.Password)
 	if err != nil {
 		return nil, err
 	}
 	return &UserModel{Password: hashedPassword, Username: dto.Username}, nil
+}
+
+type LoginUserDTO struct {
+	Username string `json:"username" binding:"required" valid:"required~Username is required"`
+	Password string `json:"password" binding:"required" valid:"sixToEightDigitAlphanumericPasswordValidator~Password must be between 6 to 8 alphanumeric characters"`
+}
+
+type ChangePasswordDTO struct {
+	OldPassword string `json:"oldPassword" binding:"required" valid:"sixToEightDigitAlphanumericPasswordValidator~Password must be between 6 to 8 alphanumeric characters"`
+	NewPassword string `json:"newPassword" binding:"required" valid:"sixToEightDigitAlphanumericPasswordValidator~Password must be between 6 to 8 alphanumeric characters"`
 }
