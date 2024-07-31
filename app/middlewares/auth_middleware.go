99f4c38c00f974c3a2017b2da33a5411ef27e5b9
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-31 16:23:10 +0630

    update(): file rename refactor and move user verification logic inside middleware

---
+++ app/middlewares/auth_middleware.go
@@ -0,0 +1,56 @@
+package middlewares
+
+import (
+	"errors"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kaungmyathan22/golang-invoice-app/app/common"
+	"github.com/kaungmyathan22/golang-invoice-app/app/lib"
+	"github.com/kaungmyathan22/golang-invoice-app/app/user"
+)
+
+func AuthMiddleware(userStorage user.UserStorage) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+		log.Println(authHeader)
+		if authHeader == "" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is missing"})
+			ctx.Abort()
+			return
+		}
+
+		tokenString := strings.TrimSpace(strings.Replace(authHeader, "Bearer", "", 1))
+		if tokenString == "" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization token is missing"})
+			ctx.Abort()
+			return
+		}
+
+		claims, err := lib.VerifyToken(tokenString)
+
+		if err != nil {
+			log.Println(err.Error())
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			ctx.Abort()
+			return
+		}
+
+		user_model, err := userStorage.GetById(claims.UserID)
+		if err != nil {
+			if errors.Is(err, user.ErrUserNotFound) {
+				ctx.JSON(http.StatusUnauthorized, common.GetUnauthorizedResponse("user with given id not found."))
+				ctx.Abort()
+				return
+			}
+			ctx.JSON(http.StatusInternalServerError, common.GetInternalServerErrorResponse("something went wrong"))
+			ctx.Abort()
+			return
+		}
+		ctx.Set("user", user_model)
+		ctx.Next()
+
+	}
+}
