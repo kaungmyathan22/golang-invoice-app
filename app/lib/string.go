4aa04f4f4240a3d25795d5a246f375334a2c4a0a
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-08-06 13:00:25 +0630

    feat(): order no genration logic added

--- app/lib/string.go
+++ app/lib/string.go
@@ -1,7 +1,9 @@
 package lib
 
 import (
+	"crypto/rand"
 	"fmt"
+	"math/big"
 	"strings"
 	"time"
 )
@@ -12,4 +14,30 @@ func GenerateSlug(name string) string {
 
 func GenerateUniqueSlug(slug string) string {
 	return fmt.Sprintf("%s-%d", slug, time.Now().UnixNano())
+}
+
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func GenerateRandomString(n int) (string, error) {
+	result := make([]byte, n)
+	for i := range result {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		if err != nil {
+			return "", err
+		}
+		result[i] = letters[num.Int64()]
+	}
+	return string(result), nil
+}
+
+func GenerateOrderNumber() (string, error) {
+	part1, err := GenerateRandomString(4)
+	if err != nil {
+		return "", err
+	}
+	part2, err := GenerateRandomString(4)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s-%s", part1, part2), nil
 }
