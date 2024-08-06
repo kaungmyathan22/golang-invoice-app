147956603b941e983665b9799337791cf7efbc38
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-08-06 10:56:46 +0630

    update(category): genreate slug before saving to db

--- app/category/cateogry_model.go
+++ app/category/cateogry_model.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"time"
 
+	"github.com/kaungmyathan22/golang-invoice-app/app/lib"
 	"gorm.io/gorm"
 )
 
@@ -14,7 +15,8 @@ var (
 
 type CategoryModel struct {
 	ID        uint   `gorm:"primaryKey"`
-	Name      string `gorm:"column:username;unique;not null"`
+	Name      string `gorm:"column:name;not null"`
+	Slug      string `gorm:"column:slug;unique;not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
@@ -24,10 +26,24 @@ func (CategoryModel) TableName() string {
 	return "category"
 }
 
+func (p *CategoryModel) BeforeCreate(tx *gorm.DB) (err error) {
+	p.Slug = lib.GenerateSlug(p.Name)
+	for {
+		var count int64
+		tx.Model(&CategoryModel{}).Where("slug = ?", p.Slug).Count(&count)
+		if count == 0 {
+			break
+		}
+		p.Slug = lib.GenerateUniqueSlug(p.Slug)
+	}
+	return nil
+}
+
 func (model *CategoryModel) ToEntity() *CategoryEntity {
 	return &CategoryEntity{
 		ID:        model.ID,
 		Name:      model.Name,
+		Slug:      model.Slug,
 		CreatedAt: model.CreatedAt,
 		UpdatedAt: model.UpdatedAt,
 		DeletedAt: model.DeletedAt,
