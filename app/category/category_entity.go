b2cfc670887980f08e3b8c6be842aa37c3790064
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-08-06 10:56:23 +0630

    update(): DRY use to entity method from model

--- app/category/category_entity.go
+++ app/category/category_entity.go
@@ -9,17 +9,12 @@ import (
 type CategoryEntity struct {
 	ID        uint           `json:"id"`
 	Name      string         `json:"name"`
+	Slug      string         `json:"slug"`
 	CreatedAt time.Time      `json:"createdAt"`
 	UpdatedAt time.Time      `json:"updatedAt"`
 	DeletedAt gorm.DeletedAt `json:"deletedAt"`
 }
 
 func CategoryEntityFromCategoryModel(model *CategoryModel) *CategoryEntity {
-	return &CategoryEntity{
-		ID:        model.ID,
-		CreatedAt: model.CreatedAt,
-		UpdatedAt: model.UpdatedAt,
-		DeletedAt: model.DeletedAt,
-		Name:      model.Name,
-	}
+	return model.ToEntity()
 }
